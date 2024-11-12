e48eb8e7f75d3ba16babb5a61451c92adece2fbf
Kaisa <raja.kaisa@gmail.com>
2024-11-12 11:19:23 +0200

    MyNew Master branch first commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello people!");
-        String text = "New world";
+        System.out.println("Hello, my new people!");
+        String text = "Today is brand new world";
         System.out.println(text);
 
-        int number = 10;
+        int number = 13;
         System.out.println(number);
     }
 }
