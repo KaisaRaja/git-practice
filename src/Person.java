4e2baa9a89484a3df82ce0c741ecafcd34b6c32d
Kaisa <raja.kaisa@gmail.com>
2024-11-12 10:05:52 +0200

    Initial project commit

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Person {
     public static void main(String[] args) {
         System.out.println("Hello, person!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
     }
 }
